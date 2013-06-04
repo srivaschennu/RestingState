c09eedf50cc73a2354cc1c3769e055d0cb4795b0
Srivas Chennu <srivas@gmail.com>
2013-06-04 23:00:11 +0000

    Checked in Wed Jun  5 00:00:11 BST 2013
    git-svn-id: file:///Users/chennu/Dropbox/Sources/RestingState@29 55f8793d-2db4-4cdb-aea5-cfa62d2d70cf

--- calcpcoh.m
+++ calcpcoh.m
@@ -7,7 +7,7 @@ chann2
 [coh,mcoh,timesout,freqsout,cohboot,cohangles,...
 allcoher,alltfX,alltfY,cohbootall] = ...
 pop_newcrossf( EEG, 1, chann1, chann2, [EEG.xmin  EEG.xmax] * 1000, 0 ,...@ 0 dc fait fft au lieu de l'autre. permet de commencer analyse a 0.5Hz. 1 : wavelet dc mieux, commence aussi a 0.5hz comparer
-    'type', 'coher','alpha',0.001,'padratio', 1,'plotamp','off','plotphase','off');
+    'type', 'phasecoher','alpha',0.001,'padratio', 1,'plotamp','off','plotphase','off');
 
 % for f = 1:size(coh,1)
 %     coh(f,coh(f,:) <= cohboot(f)) = 0;
