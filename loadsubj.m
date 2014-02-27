9df8edd92d587f4ea3153e9ed347586c6ed42ec5
Srivas Chennu <srivas@gmail.com>
2014-02-27 00:00:23 +0000

    Checked in Thu Feb 27 00:00:22 GMT 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/RestingState@102 55f8793d-2db4-4cdb-aea5-cfa62d2d70cf

--- loadsubj.m
+++ loadsubj.m
@@ -1,72 +1,72 @@
-subjlist = {
-'p0713_restingstate'
-'p0813_restingstate'
-'p0913_restingstate'
-'p1013_restingstate'
-'p0114_restingstate'
-    };
+% subjlist = {
+%     'p0713_restingstate'
+%     'p0813_restingstate'
+%     'p0913_restingstate'
+%     'p1013_restingstate'
+%     'p0114_restingstate'
+%     };
 
-testlist = {
-% 	'p0311_restingstate1'	0	7	0	''	0
-    'NW_restingstate'   2  25   2     ''    2
-    };
+% testlist = {
+%     % 	'p0311_restingstate1'	0	7	0	''	0
+%     'NW_restingstate'   2  25   2     ''    2
+%     };
 
 patlist = {
     %
     % patients
     
-	'p0311_restingstate1'	0	7	0	''	0
-	'p0411_restingstate1'	0	7	0	''	1
-	'p1611_restingstate'	0	7	1	''	1
-	'p0510V2_restingstate'	0	7	0	''	1
-	'p1311_restingstate'	0	8	0	''	1
-	'p2011_restingstate'	0	7	0	''	0
-	'p0612_restingstate'	0	8	0	''	1
-	'p71v3_restingstate'	0	8	0	''	1
-	'p0712_restingstate'	0	7	0	''	0
-	'p0113_restingstate'	0	7	1	''	1
-	'p0313_restingstate'	0	7	1	''	1
-	'p0613_restingstate'	0	8	0	''	1
-	'p0812_restingstate1'	0	7	1	''	1
-	'p0611_restingstate'	1	10	1	''	0
-	'p0312_restingstate'	1	8	1	''	0
-	'p0211_restingstate1'	1	12	1	''	1
-	'p0511_restingstate'	1	9	1	''	0
-	'p0811_restingstate'	1	10	0	''	0
-	'p0911_restingstate'	1	11	1	''	1
-	'p1011_restingstate'	1	10	0	''	1
-	'p1511_restingstate'	1	10	1	''	1
-	'p1811_restingstate'	1	12	0	''	1
-	'p1911_restingstate'	1	9	0	''	1
-	'p0112_restingstate'	1	9	1	''	0
-	'p0212_restingstate'	1	12	0	''	1
-	'p0512_restingstate'	1	8	1	''	1
-	'p0710V2_restingstate'	1	14	1	''	1
-	'p0711_restingstate'	1	15	0	''	1
-	'p1711_restingstate'	1	17	0	''	0
-	'p1012_restingstate'	1	13	1	''	1
-	'p0213_restingstate'	1	19	1	''	1
-	'p0413_restingstate'	1	14	0	''	1
+    'p0311_restingstate1'	0	7	0	''	0	0.117967
+    'p0411_restingstate1'	0	7	0	''	1	0.153460
+    'p1611_restingstate'	0	7	1	''	1	0.091762
+    'p0510V2_restingstate'	0	7	0	''	1	0.093890
+    'p1311_restingstate'	0	8	0	''	1	0.075433
+    'p2011_restingstate'	0	7	0	''	0	0.090078
+    'p0612_restingstate'	0	8	0	''	1	0.086983
+    'p71v3_restingstate'	0	8	0	''	1	0.106934
+    'p0712_restingstate'	0	7	0	''	0	0.087559
+    'p0113_restingstate'	0	7	1	''	1	0.108512
+    'p0313_restingstate'	0	7	1	''	1	0.083591
+    'p0613_restingstate'	0	8	0	''	1	0.101270
+    'p0812_restingstate1'	0	7	1	''	1	0.089513
+    'p0611_restingstate'	1	10	1	''	0	0.104962
+    'p0312_restingstate'	1	8	1	''	0	0.052222
+    'p0211_restingstate1'	1	12	1	''	1	0.115601
+    'p0511_restingstate'	1	9	1	''	0	0.090693
+    'p0811_restingstate'	1	10	0	''	0	0.074285
+    'p0911_restingstate'	1	11	1	''	1	0.112739
+    'p1011_restingstate'	1	10	0	''	1	0.115158
+    'p1511_restingstate'	1	10	1	''	1	0.077297
+    'p1811_restingstate'	1	12	0	''	1	0.099947
+    'p1911_restingstate'	1	9	0	''	1	0.084377
+    'p0112_restingstate'	1	9	1	''	0	0.088890
+    'p0212_restingstate'	1	12	0	''	1	0.110015
+    'p0512_restingstate'	1	8	1	''	1	0.106777
+    'p0710V2_restingstate'	1	14	1	''	1	0.144079
+    'p0711_restingstate'	1	15	0	''	1	0.102792
+    'p1711_restingstate'	1	17	0	''	0	0.103720
+    'p1012_restingstate'	1	13	1	''	1	0.108883
+    'p0213_restingstate'	1	19	1	''	1	0.111607
+    'p0413_restingstate'	1	14	0	''	1	0.106301
     
-%     %follow-ups
-%     'p0311V2_restingstate'  0   8   0   'p0311_restingstate1'   0
-%     'p1311v2_restingstate'  0   8   0   'p1311_restingstate'    1
-%     'p0612v2_restingstate'  0   7   1   'p0612_restingstate'    1
-%     'p0712v2_restingstate'  0   7   0   'p0712_restingstate'    0
-%     'p0411V2_restingstate'  1   8   0   'p0411_restingstate1'   1 %noisy
-%     'p0512v2_restingstate'  1   8   0   'p0512_restingstate'    1 %noisy
-%     'p0511V2_restingstate'  1   10  0   'p0511_restingstate'    0 %noisy
-%     'p1511v2_restingstate'  1   10  0   'p1511_restingstate'    1
-%     'p0611v2_restingstate'  1   11  0   'p0611_restingstate'    0 %noisy
-%     'p0211V2_restingstate'	1   16  1   'p0211_restingstate1'   1
-%     'p1811v2_restingstate'  1   20  0   'p1811_restingstate'    1
+    %     %follow-ups
+    %     'p0311V2_restingstate'  0   8   0   'p0311_restingstate1'   0
+    %     'p1311v2_restingstate'  0   8   0   'p1311_restingstate'    1
+    %     'p0612v2_restingstate'  0   7   1   'p0612_restingstate'    1
+    %     'p0712v2_restingstate'  0   7   0   'p0712_restingstate'    0
+    %     'p0411V2_restingstate'  1   8   0   'p0411_restingstate1'   1 %noisy
+    %     'p0512v2_restingstate'  1   8   0   'p0512_restingstate'    1 %noisy
+    %     'p0511V2_restingstate'  1   10  0   'p0511_restingstate'    0 %noisy
+    %     'p1511v2_restingstate'  1   10  0   'p1511_restingstate'    1
+    %     'p0611v2_restingstate'  1   11  0   'p0611_restingstate'    0 %noisy
+    %     'p0211V2_restingstate'	1   16  1   'p0211_restingstate1'   1
+    %     'p1811v2_restingstate'  1   20  0   'p1811_restingstate'    1
     
     %duplicates
     % p0211_restingstate2'   2   14 duplicate
     %'p0311_restingstate2'   0   7 duplicate
     %'p0411_restingstate2'   0   7 duplicate
     
-
+    
     % 'p2111_restingstate'    1   10 %noisy
     % 'p0513_restingstate' %noisy
     
@@ -80,32 +80,32 @@ patlist = {
 ctrllist = {
     %     controls
     
-    'NW_restingstate'   2  25   2     ''    2
-    'p37_restingstate'  2  25   2     ''    2
-    'p38_restingstate'  2  25   2     ''    2
-    'p40_restingstate'  2  25   2     ''    2
-    'p41_restingstate'  2  25   2     ''    2
-    'p42_restingstate'  2  25   2     ''    2
-    'p43_restingstate'  2  25   2     ''    2
-    'p44_restingstate'  2  25   2     ''    2
-    'p45_restingstate'  2  25   2     ''    2
-    'p46_restingstate'  2  25   2     ''    2
-    'p47_restingstate'  2  25   2     ''    2
-    'p48_restingstate'  2  25   2     ''    2
-    'p49_restingstate'  2  25   2     ''    2
-    'subj01_restingstate'  2    25   2     ''    2
-    'subj02_restingstate'  2    25   2     ''    2
-    'VS_restingstate'  2  25   2     ''    2
-    'SS_restingstate'  2  25   2     ''    2
-    'SB_restingstate'  2  25   2     ''    2
-    'ML_restingstate'  2  25   2     ''    2
-    'MC_restingstate'  2  25   2     ''    2
-    'JS_restingstate'  2  25   2     ''    2
-    'ET_restingstate'  2  25   2     ''    2
-    'EP_restingstate'  2  25   2     ''    2
-    'CL_restingstate'  2  25   2     ''    2
-    'CD_restingstate'  2  25   2     ''    2
-    'AC_restingstate'  2  25   2     ''    2
+    'NW_restingstate'	2	25	2	''	2	1.000000
+    'p37_restingstate'	2	25	2	''	2	1.000000
+    'p38_restingstate'	2	25	2	''	2	1.000000
+    'p40_restingstate'	2	25	2	''	2	1.000000
+    'p41_restingstate'	2	25	2	''	2	1.000000
+    'p42_restingstate'	2	25	2	''	2	1.000000
+    'p43_restingstate'	2	25	2	''	2	1.000000
+    'p44_restingstate'	2	25	2	''	2	1.000000
+    'p45_restingstate'	2	25	2	''	2	1.000000
+    'p46_restingstate'	2	25	2	''	2	1.000000
+    'p47_restingstate'	2	25	2	''	2	1.000000
+    'p48_restingstate'	2	25	2	''	2	1.000000
+    'p49_restingstate'	2	25	2	''	2	1.000000
+    'subj01_restingstate'	2	25	2	''	2	1.000000
+    'subj02_restingstate'	2	25	2	''	2	1.000000
+    'VS_restingstate'	2	25	2	''	2	1.000000
+    'SS_restingstate'	2	25	2	''	2	1.000000
+    'SB_restingstate'	2	25	2	''	2	1.000000
+    'ML_restingstate'	2	25	2	''	2	1.000000
+    'MC_restingstate'	2	25	2	''	2	1.000000
+    'JS_restingstate'	2	25	2	''	2	1.000000
+    'ET_restingstate'	2	25	2	''	2	1.000000
+    'EP_restingstate'	2	25	2	''	2	1.000000
+    'CL_restingstate'	2	25	2	''	2	1.000000
+    'CD_restingstate'	2	25	2	''	2	1.000000
+    'AC_restingstate'	2	25	2	''	2	1.000000
     %     'jenny_restingstate'
     %     %'SaltyWater_restingstate'
     % 'p50_restingstate'
@@ -135,20 +135,20 @@ ctrllist = {
 %     %    'p1311v2_restingstate'  0   0
 %     };
 
-overnight = {
-    'p2_overnight1_noalpha_chunk1'  0
-    'p2_overnight1_noalpha_chunk2'  0
-    'p2_overnight1_noalpha_chunk3'  0
-    'p2_overnight1_noalpha_chunk4'  0
-    'p2_overnight1_noalpha_chunk5'  0
-    'p2_overnight1_noalpha_chunk6'  0
-    
-    'p2_overnight2_alpha_chunk1'    1
-    'p2_overnight2_alpha_chunk2'    1
-    'p2_overnight2_alpha_chunk3'    1
-    'p2_overnight2_alpha_chunk4'    1
-    'p2_overnight2_alpha_chunk5'    1
-    'p2_overnight2_alpha_chunk6'    1
-    };
+% overnight = {
+%     'p2_overnight1_noalpha_chunk1'  0
+%     'p2_overnight1_noalpha_chunk2'  0
+%     'p2_overnight1_noalpha_chunk3'  0
+%     'p2_overnight1_noalpha_chunk4'  0
+%     'p2_overnight1_noalpha_chunk5'  0
+%     'p2_overnight1_noalpha_chunk6'  0
+%     
+%     'p2_overnight2_alpha_chunk1'    1
+%     'p2_overnight2_alpha_chunk2'    1
+%     'p2_overnight2_alpha_chunk3'    1
+%     'p2_overnight2_alpha_chunk4'    1
+%     'p2_overnight2_alpha_chunk5'    1
+%     'p2_overnight2_alpha_chunk6'    1
+%     };
 
 allsubj = cat(1,patlist,ctrllist);
---
+++ testmean.m
@@ -0,0 +1,199 @@
+function [testdata,grp,v2idx,v1idx,pval1,stats1] = testmean(listname,conntype,bandidx,varargin)
+
+loadpaths
+
+load(sprintf('%s/%s/alldata_%s_%s.mat',filepath,conntype,listname,conntype));
+
+loadsubj
+subjlist = allsubj;
+
+param = finputcheck(varargin, {
+    'xlim', 'real', [], []; ...
+    'xtick', 'real', [], []; ...
+    'ylim', 'real', [], []; ...
+    'plotinfo', 'string', {'on','off'}, 'on'; ...
+    'legend', 'string', {'on','off'}, 'on'; ...
+    'legendposition', 'string', {}, ''; ...
+    'xlabel', 'string', {}, 'wpli'; ...
+    'exclude', 'string', {'on','off'}, 'off'; ...
+    });
+
+fontname = 'Helvetica';
+fontsize = 28;
+
+% powerdata = load(sprintf('alldata_%s.mat',listname));
+% load chanlist
+
+weiorbin = 3;
+trange = [0.5 0.1];
+
+crs = cell2mat(subjlist(:,7));
+tennis = logical(cell2mat(subjlist(:,4)));
+
+bands = {
+    'Delta'
+    'Theta'
+    'Alpha'
+    'Beta'
+    'Gamma'
+    };
+
+v1idx = zeros(size(subjlist,1),1);
+for s = 1:size(subjlist,1)
+    if ~isempty(subjlist{s,5})
+        v1idx(s) = find(strcmp(subjlist{s,5},subjlist(:,1)));
+    end
+end
+v2idx = logical(v1idx);
+v1idx = nonzeros(v1idx);
+
+testdata = mean(mean(allcoh(:,bandidx,:,:),3),4);
+
+%% test patients vs controls group difference
+% [pval1,~,stats] = ranksum(testdata(grp == 2),testdata((grp == 0 | grp == 1) & ~v2idx));
+[~,pval1,~,stats1] = ttest2(testdata(grp == 2),testdata((grp == 0 | grp == 1) & ~v2idx),[],[],'unequal');
+fprintf('%s band: Ctrl %.2f, Pat %.2f, t = %.2f, p = %.3f.\n',...
+    bands{bandidx},mean(testdata(grp == 2)),mean(testdata((grp == 0 | grp == 1) & ~v2idx)),stats1.tstat,pval1);
+
+%% compare vs to mcs patients
+[~,pval2,~,stats2] = ttest2(testdata(grp == 1 & ~v2idx),testdata(grp == 0 & ~v2idx),[],[],'unequal');
+fprintf('MCS vs. VS: t = %.2f, p = %.3f.\n',stats2.tstat,pval2);
+
+%% compare measure between VS imagers and non-imagers
+% [pval,~,stats] = ranksum(testdata((grp == 0 | grp == 1) & ~v2idx & ~tennis),testdata((grp == 0 | grp == 1) & ~v2idx & tennis));
+[~,pval2,~,stats2] = ttest2(testdata(grp == 0 & ~v2idx & ~tennis),testdata(grp == 0 & ~v2idx & tennis),[],[],'unequal');
+fprintf('VS Imagers vs non-imagers: t = %.2f, p = %.3f.\n',stats2.tstat,pval2);
+
+%% correlate patients with crs scores
+
+datatable = sortrows(cat(2,...
+    crs((grp == 0 | grp == 1) & ~v2idx),...
+    testdata((grp == 0 | grp == 1) & ~v2idx),...
+    tennis((grp == 0 | grp == 1) & ~v2idx),...
+    grp((grp == 0 | grp == 1) & ~v2idx)),...
+    2);
+%     powerdata((grp == 0 | grp == 1) & ~v2idx)),...
+mdl = LinearModel.fit(datatable(:,2),datatable(:,1),'RobustOpts','on');
+fprintf('%s: R2 = %.2f, p = %.3f.\n',bands{bandidx},mdl.Rsquared.Adjusted,doftest(mdl));
+exmdl = LinearModel.fit(datatable(:,2),datatable(:,1),'RobustOpts','on','Exclude',find(datatable(:,4) == 0));
+fprintf('%s (excl): R2 = %.2f, p = %.3f.\n',bands{bandidx},exmdl.Rsquared.Adjusted,doftest(exmdl));
+
+% test with power covariate
+% exmdl = LinearModel.fit(datatable(:,[2 5]),datatable(:,1),'RobustOpts','on','Exclude',find(datatable(:,4) == 0 & datatable(:,3) == 1))
+% fprintf('%s %s (excl with power): R2 = %.2f, p = %.3f.\n',bands{bandidx},measure,exmdl.Rsquared.Adjusted,doftest(exmdl));
+
+% [rho, pval] = corr(datatable(:,1),datatable(:,2),'type','spearman');
+% fprintf('Spearman rho = %.2f, p = %.3f.\n',rho,pval);
+
+
+figure('Color','white');
+hold all
+%VS
+legendoff(scatter(datatable(datatable(:,4) == 0 & datatable(:,3) == 0,2), ...
+    datatable(datatable(:,4) == 0 & datatable(:,3) == 0,1),'red'));
+legendoff(scatter(datatable(datatable(:,4) == 0 & datatable(:,3) == 1,2), ...
+    datatable(datatable(:,4) == 0 & datatable(:,3) == 1,1),'red','filled'));
+% legendoff(scatter(datatable(datatable(:,4) == 0,2),datatable(datatable(:,4) == 0,1),'red','filled'));
+%MCS
+legendoff(scatter(datatable(datatable(:,4) == 1 & datatable(:,3) == 0,2), ...
+    datatable(datatable(:,4) == 1 & datatable(:,3) == 0,1),'blue'));
+legendoff(scatter(datatable(datatable(:,4) == 1 & datatable(:,3) == 1,2), ...
+    datatable(datatable(:,4) == 1 & datatable(:,3) == 1,1),'blue','filled'));
+% legendoff(scatter(datatable(datatable(:,4) == 1,2),datatable(datatable(:,4) == 1,1),'blue','filled'));
+
+p0311idx = 1;
+legendoff(scatter(testdata(p0311idx),crs(p0311idx),100,'black','LineWidth',2));
+p1611idx = 3;
+legendoff(scatter(testdata(p1611idx),crs(p1611idx),100,'black','LineWidth',2));
+
+b = mdl.Coefficients.Estimate;
+plot(datatable(:,2),b(1)+b(2)*datatable(:,2),'-','Color','black',...
+    'Display',sprintf('R^2 = %.2f, p = %.3f',mdl.Rsquared.Adjusted,doftest(mdl)));
+b = exmdl.Coefficients.Estimate;
+plot(datatable(datatable(:,4) == 1,2),b(1)+b(2)*datatable(datatable(:,4) == 1,2),'--','Color','black',...
+    'Display',sprintf('R^2 = %.2f, p = %.3f',exmdl.Rsquared.Adjusted,doftest(exmdl)));
+
+set(gca,'FontName',fontname,'FontSize',fontsize);
+if ~isempty(param.ylim)
+    set(gca,'YLim',param.ylim);
+end
+if ~isempty(param.xlim)
+    set(gca,'XLim',param.xlim);
+end
+
+xlabel(param.xlabel,'FontName',fontname,'FontSize',fontsize);
+if strcmp(param.plotinfo,'on')
+    ylabel('CRS-R score','FontName',fontname,'FontSize',fontsize);
+else
+    ylabel(' ','FontName',fontname,'FontSize',fontsize);
+end
+if strcmp(param.legend,'on')
+    leg_h = legend('show');
+    if isempty(param.legendposition)
+        set(leg_h,'Location','Best');
+    else
+        set(leg_h,'Location',param.legendposition);
+    end
+    txt_h = findobj(leg_h,'type','text');
+    set(txt_h,'FontSize',fontsize-6,'FontWeight','bold')
+    legend('boxoff');
+end
+
+export_fig(sprintf('figures/crscorr_mean_%s.eps',bands{bandidx}));
+close(gcf);
+
+futable = sortrows(cat(2,...
+    crs(v2idx)-crs(v1idx),...
+    testdata(v1idx)),...
+    2);
+%     powerdata(v1idx)),...
+
+% %% correlate follow-ups
+% % [rho, pval] = corr(futable(:,1),futable(:,2),'type','spearman');
+% % fprintf('Follow-up: Spearman rho = %.2f, p = %.3f.\n',rho,pval);
+% 
+% mdl = LinearModel.fit(futable(:,2),futable(:,1),'RobustOpts','off');
+% fprintf('%s follow-up: R2 = %.2f, p = %.3f.\n',bands{bandidx},mdl.Rsquared.Adjusted,doftest(mdl));
+% % powmdl = LinearModel.fit(futable(:,[2 3]),futable(:,1),'RobustOpts','off');
+% % fprintf('%s follow-up with power: R2 = %.2f, p = %.3f.\n',bands{bandidx},powmdl.Rsquared.Adjusted,doftest(powmdl));
+% 
+% figure('Color','white');
+% hold all
+% legendoff(scatter(futable(:,2),futable(:,1),'filled'));
+% 
+% b = mdl.Coefficients.Estimate;
+% plot(futable(:,2),b(1)+b(2)*futable(:,2),'-','Color','black',...
+%     'Display',sprintf('R^2 = %.2f, p = %.3f',mdl.Rsquared.Adjusted,doftest(mdl)));
+% % b = powmdl.Coefficients.Estimate;
+% % plot(futable(:,2),b(1)+b(2)*futable(:,2),'--','Color','black',...
+% %     'Display',sprintf('R^2 = %.2f, p = %.3f',powmdl.Rsquared.Adjusted,doftest(powmdl)));
+% 
+% set(gca,'FontName',fontname,'FontSize',fontsize);
+% if ~isempty(param.xlim)
+%     set(gca,'XLim',param.xlim);
+% end
+% if ~isempty(param.ylim)
+%     set(gca,'YLim',param.ylim);
+% end
+% xlabel(sprintf('Visit1 %s %s',bands{bandidx},measure),'FontName',fontname,'FontSize',fontsize);
+% if strcmp(param.plotinfo,'on')
+%     ylabel('V2 CRS-R - V1 CRS-R','FontName',fontname,'FontSize',fontsize);
+% else
+%     ylabel(' ','FontName',fontname,'FontSize',fontsize);
+% end
+% if strcmp(param.legend,'on')
+%     leg_h = legend('show');
+%     if isempty(param.legendposition)
+%         set(leg_h,'Location','Best');
+%     else
+%         set(leg_h,'Location',param.legendposition);
+%     end
+%     txt_h = findobj(leg_h,'type','text');
+%     set(txt_h,'FontSize',fontsize-6,'FontWeight','bold')
+%     legend('boxoff');
+% end
+% 
+% export_fig(sprintf('figures/measurefu_%s_%s.eps',measure,bands{bandidx}));
+% close(gcf);
+% 
+end
