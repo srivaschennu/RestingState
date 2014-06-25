6d96114ae6a9456e08cbdb3f1bd7a878591b5b1b
Srivas Chennu <srivas@gmail.com>
2014-06-25 23:00:16 +0000

    Checked in Thu Jun 26 00:00:16 BST 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/RestingState@141 55f8793d-2db4-4cdb-aea5-cfa62d2d70cf

--- ploteog.m
+++ ploteog.m
@@ -8,19 +8,17 @@ ylimits = [0 5];
 
 EEG = pop_loadset('filepath',filepath,'filename',[basename '_eog.set']);
 
-EEG = pop_rmbase(EEG,[]);
-
 for c = 1:EEG.nbchan
     eogrsd(c,:) = (squeeze(std(EEG.data(c,:,:),[],2)) / mean(squeeze(std(EEG.data(c,:,:),[],2))));
 end
 eogrsd = mean(eogrsd,1);
-
 
 figure('Color','white');
 figpos = get(gcf,'Position');
 
 % figpos(4) = figpos(4)/3.8;
 figpos(4) = figpos(4)/5;
+
 set(gcf,'Position',figpos);
 
 plot(eogrsd,'LineWidth',1);
@@ -28,9 +26,9 @@ plot(eogrsd,'LineWidth',1);
 set(gca,'YLim',ylimits);
 
 set(gca,'XTick',[],'YTick',[]);
-% set(gca,'YTick',ylimits,'FontName',fontname,'FontSize',fontsize);
-% xlabel('Time (sec)','FontName',fontname,'FontSize',fontsize-2);
-% xlabel('RSD','FontName',fontname,'FontSize',fontsize-2);
+% set(gca,'YTick',ylimits,'FontName',fontname,'FontSize',fontsize-2);
+% xlabel('Time (sec)','FontName',fontname,'FontSize',fontsize);
+% ylabel('Norm. SD','FontName',fontname,'FontSize',fontsize);
 
 box off
 
