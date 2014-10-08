7a1a41c9e7bc84c48d02a827ead65df904c08983
Srivas Chennu <srivas@gmail.com>
2014-10-08 23:00:15 +0000

    Checked in Thu Oct  9 00:00:14 BST 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/RestingState@157 55f8793d-2db4-4cdb-aea5-cfa62d2d70cf

--- plotmeangraph.m
+++ plotmeangraph.m
@@ -34,18 +34,18 @@ grp = grp(~v2idx);
 allcoh = allcoh(~v2idx,:,:,:,:);
 groups = unique(grp);
 
-% grouplist = {
-%     'p0311_restingstate1'
-%     'p0411_restingstate1'
-%     'p1611_restingstate'
-%     'CristianCabezas'
-%     };
+grouplist = {
+    'p0311_restingstate1'
+    'p0411_restingstate1'
+    'p1611_restingstate'
+    'CristianCabezas'
+    };
 
 group = 1:length(grouplist);
 
 for g = group
-    groupcoh(g,:,:) = squeeze(mean(allcoh(grp == groups(g),bandidx,:,:),1));
-%     groupcoh(g,:,:) = squeeze(mean(allcoh(strcmp(grouplist{g},subjlist(:,1)),bandidx,:,:),1));
+%     groupcoh(g,:,:) = squeeze(mean(allcoh(grp == groups(g),bandidx,:,:),1));
+    groupcoh(g,:,:) = squeeze(mean(allcoh(strcmp(grouplist{g},subjlist(:,1)),bandidx,:,:),1));
     threshcoh(g,:,:) = threshold_proportional(squeeze(groupcoh(g,:,:)),1-plotqt);
     for c = 1:size(threshcoh,2)
         groupdeg(g,c) = sum(threshcoh(g,c,:))/(size(threshcoh,2)-1);
