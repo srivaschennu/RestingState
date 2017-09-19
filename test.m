7d67d676565dfca8c65297d1705645e353054bdb
Srivas Chennu <srivas@gmail.com>
2017-09-19 23:46:11 +0100

    Checked in Tue 19 Sep 2017 23:46:11 BST

--- test.m
+++
@@ -1,9 +0,0 @@
-for s = 1:size(allsubj,1)
-    data1 = load([filepath 'wpli-ploscompbio/' allsubj{s,1} 'wplifdr.mat'],'matrix');
-    data2 = load([filepath 'wpli/' allsubj{s,1} 'wplifdr.mat'],'matrix');
-    for f = 1:size(data1.matrix,1)
-        if find(abs(data1.matrix(f,:)) ~= abs(data2.matrix(f,:)))
-            fprintf('%s: %d not equal\n',allsubj{s,1},f)
-        end
-    end
-end
